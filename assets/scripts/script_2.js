d1cc8134a07f57452866af5728e79d0fe3da9b0c
Afika Nyati <anyati@mit.edu>
2018-04-11 15:06:48 -0400

    changed scale for other variables in treemap

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -176,12 +176,16 @@ d3.json("assets/data/us.json", function(data) {
             treemap(root
             .sum(function (d) {
                 if (treeSumSortType == "number") {
+                    color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84"]);
                     return d["Total College"];
                 } else if (treeSumSortType == "percent") {
+                    color = d3.scaleLinear().domain([0, 1/4*50, 2/4*50, 3/4*50, 50]).range(["#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84"]);
                     return d["Percent College"];
                 } else if (treeSumSortType == "male") {
+                    color = d3.scaleLinear().domain([0, 1/4*50, 2/4*50, 3/4*50, 50]).range(["#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84"]);
                     return d["Percent College - Male"];
                 } else {
+                    color = d3.scaleLinear().domain([0, 1/4*50, 2/4*50, 3/4*50, 50]).range(["#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84"]);
                     return d["Percent College - Female"];
                 }
 
